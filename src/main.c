e0881503283336f926d7777bbbe7368c059244ef
Ellen Marie Dash <me@duckie.co>
2018-07-09 21:39:48 -0400

    Add cadel_y_intercept() and cadel_slope().

--- src/main.c
+++ src/main.c
@@ -2,6 +2,40 @@
 #include <stdint.h>
 #include <stddef.h>
 
+// returns +b+ in
+//   b = y - (m * x)
+// where
+//   m is the slope
+//   x is the x coordinate
+//   y is the y coordinate
+//   b is the y intercept
+uint32_t cadel_y_intercept(CadelDisplay *display, uint32_t slope,
+        CadelPoint point)
+{
+    return (point.y - (slope * point.x));
+}
+
+// returns +m+ in
+//   m = (y1 - y2) / (x1 - x2)
+// where
+//   m is the slope
+//   (x1, y1) is the first coordinate
+//   (x2, y2) is the second coordinate
+int32_t cadel_slope(CadelPoint a, CadelPoint b)
+{
+    uint32_t x1 = a.x;
+    uint32_t y1 = a.y;
+    uint32_t x2 = b.x;
+    uint32_t y2 = b.y;
+
+    if (y1 > y2) {
+        return cadel_slope(b, a);
+    }
+
+    return (y2 - y1) / (x2 - x1);
+}
+
+
 int32_t cadel_abs(int32_t n)
 {
     return ((n < 0) ? -n : n);
