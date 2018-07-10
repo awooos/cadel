5b98f7defd7545f91fc53a74c72a7d3601f6a46c
Ellen Marie Dash <me@duckie.co>
2018-07-10 15:26:08 -0400

    ?!

--- src/main.c
+++ src/main.c
@@ -14,28 +14,6 @@ uint8_t cadel_get_pixel(CadelCanvas canvas, int64_t x, int64_t y)
     return canvas.data[(y * canvas.width) + x];
 }
 
-// Set the value of the pixel at +(x, y)+ on +canvas+ to +val+.
-void cadel_set_pixel(CadelCanvas canvas, int64_t x, int64_t y, int8_t val)
-{
-    canvas.data[(y * canvas.width) + x] = val;
-}
-
-// Renders a vertical line to a CadelCanvas.
-void cadel_render_vertical_line(CadelCanvas canvas,
-        int64_t x, int64_t y, int64_t length)
-{
-    // If we get a negative length, then invert
-    // the y coordinate and negate the length.
-    if (length < 0) {
-        length = -length;
-        y = y - length + 1;
-    }
-
-    for (int64_t idx = 0; idx < length; idx++) {
-        cadel_set_pixel(canvas, x, y + idx, 1);
-    }
-}
-
 void cadel_render_line(CadelCanvas canvas, CadelPoint l, CadelPoint r)
 {
     // Ensure +l+ is to the left of +r+ by swapping argument order if needed.
@@ -49,14 +27,23 @@ void cadel_render_line(CadelCanvas canvas, CadelPoint l, CadelPoint r)
     double  m = (double)(r.y - l.y) / (r.x - l.x);
     int64_t b = l.y - (m * l.x);
 
-    for (int64_t x = l.x; x <= r.x; x++) {
+    for (int64_t x = l.x + 1; x <= r.x; x++) {
         int64_t last_y = (m * (x - 1)) + b;
         int64_t y = (m * x) + b;
 
-        // Draw the exact pixel.
-        cadel_set_pixel(canvas, x, y, 1);
-        // Draw adjacent pixels if needed.
-        cadel_render_vertical_line(canvas, x, y, last_y - y);
+        int64_t low = y + 1;
+        int64_t high = last_y + 1;
+        if (y > last_y) {
+            low = last_y;
+            high = y;
+        }
+        if (low < high) {
+            low++;
+        }
+
+        for (int64_t new_y = low; new_y <= high; new_y++) {
+            canvas.data[(new_y * canvas.width) + x] = 1;
+        }
     }
 }
 
