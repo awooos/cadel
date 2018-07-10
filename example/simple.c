539eee469c12c3f7c95f8ae2ed993e8ccb588516
Ellen Marie Dash <me@duckie.co>
2018-07-10 15:57:34 -0400

    ?!

--- example/simple.c
+++ example/simple.c
@@ -4,29 +4,43 @@
 #include <cadel.h>
 #include <stdio.h>
 
-int main()
-{
-    CadelCanvas canvas = cadel_canvas(20, 10);
-
-    CadelObject triangle = cadel_object(
-            {15, 2},
-            {2, 6},
-            {18, 6},
-            {15, 2});
-
-    cadel_clear(canvas);
-    cadel_render_object(canvas, triangle);
-
+void print_canvas(CadelCanvas canvas) {
     for (size_t y = 0; y < canvas.height; y++) {
         for (size_t x = 0; x < canvas.width; x++) {
             if (cadel_get_pixel(canvas, x, y) == 0) {
-                printf("-");
+                printf(" ");
             } else {
                 printf("X");
             }
         }
         printf("\n");
     }
+}
+
+int main()
+{
+    CadelCanvas canvas = cadel_canvas(20, 10);
+
+    CadelObject triangle = cadel_object(
+            {15, 2},
+            { 2, 6},
+            {18, 6},
+            {15, 2});
+
+    CadelObject parallelogram = cadel_object(
+            { 6, 1},
+            {18, 1},
+            {11, 8},
+            { 0, 8},
+            { 6, 1});
+
+    cadel_clear(canvas);
+    cadel_render_object(canvas, triangle);
+    print_canvas(canvas);
+
+    cadel_clear(canvas);
+    cadel_render_object(canvas, parallelogram);
+    print_canvas(canvas);
 
     return 0;
 }
